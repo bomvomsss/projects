84dcbd3025daa2f91fa17c3be08e9a1937630704
Kyla Kim <109022128+bomvomsss@users.noreply.github.com>
2024-11-01 20:06:23 +0900

    카카오맵 api 연결

--- matjip/src/App.js
+++ matjip/src/App.js
@@ -2,19 +2,24 @@ import './styles/App.scss';
 import {useState} from "react";
 import itemData from './data.js';
 import Item from './components/Item.jsx';
+import KakaoMap from './components/KakaoMap.jsx';
 import { Row, Col } from "react-bootstrap";
 
 function App() {
-  let [cardItem, setCardItem] = useState(itemData);
+  const [cardItem, setCardItem] = useState(itemData);
+  const [btnOpen, setBtnOpen] = useState(false);
 
   return (
     <div className="App">
       <div className="list">
+        <div className={`${btnOpen ? "mapBg on" : "mapBg"}`}>
+          <KakaoMap/>
+        </div>
         <Row xs={1} md={4}>
         {cardItem.map((item, i) => {
           return (
             <Col key={i}>
-              <Item cardItem={item} index={i}></Item>
+              <Item btnOpen={btnOpen} setBtnOpen={setBtnOpen} cardItem={item} index={i}></Item>
             </Col>
             )
           })}
--- matjip/src/components/Item.jsx
+++ matjip/src/components/Item.jsx
@@ -1,12 +1,16 @@
 import {Button, Card} from 'react-bootstrap';
+import {useState} from "react";
 
 function Item(props){
-    let btnTxt = "지도 보기";
+  let btnTxt = "지도 보기";
 
+  let toggleMap = () => {
+    props.setBtnOpen(!props.btnOpen);
+    console.log('hey')
+  }
   return(
     <>
       <Card style={{ width: '23rem' }}>
-        {/* <Card.Img variant="top" src={props.cardItem.img} alt=""/> */}
         <div className="card-img-top">
           <img src={props.cardItem.img} alt="" />
         </div>
@@ -16,7 +20,7 @@ function Item(props){
             {props.cardItem.content}
           </Card.Text>
           <div className='d-grid gap-2'>
-            <Button variant="primary">{btnTxt}</Button>
+            <Button variant="primary" onClick={toggleMap} >{btnTxt}</Button>
           </div>
         </Card.Body>
       </Card>
